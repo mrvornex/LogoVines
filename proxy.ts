d3f534044d5751d79b5b22123e98de9d7450c3fb
mrvornex <bilalali.office.pk@gmail.com>
2026-05-23 13:32:30 +0500

    Updated

--- proxy.ts
+++ proxy.ts
@@ -15,7 +15,7 @@ export async function proxy(req: NextRequest) {
       return NextResponse.next();
     } catch {
       return NextResponse.redirect(new URL("/login", req.url));
-    }
+    } 
   }
  
   // Protected user routes
